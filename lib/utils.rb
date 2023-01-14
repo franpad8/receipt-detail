f53ca20931e6d390dd4c5cc580a9d2e6cefa833a
Franklin Padilla <fpadilla@buk.cl>
2023-01-14 20:59:33 -0300

    refactor: move parsing and print methods to a helper file

---
+++ lib/utils.rb
@@ -0,0 +1,28 @@
+# Utility/Helper methods
+
+def parse_order_lines lines
+  regex = /^(\d+)\s+([\w\s]+)\sat\s+(\d+\.\d+)$/
+  lines = lines.split("\n")
+  raise_format_error if lines.empty?
+
+  lines.map do |line|
+    match = line.match(regex)
+    raise_format_error if match.nil? || match.length != 4
+
+    quantity, name, price = match.to_a[1..-1]
+
+    { quantity: quantity.to_i, name: name, price: price.to_f.round(2) }
+  end
+end
+
+def print_receipt receipt
+  receipt[:items].each do |item|
+    puts "#{item[:quantity]} #{item[:name]}: #{item[:total]}"
+  end
+  puts "Sales Taxes: #{receipt[:tax_total]}"
+  puts "Total: #{receipt[:total]}"
+end
+
+def raise_format_error
+  raise StandardError.new("Invalid input format")
+end
--- main.rb
+++ main.rb
@@ -1,4 +1,5 @@
 #!/usr/bin/env ruby
+require_relative 'lib/utils'
 require_relative 'lib/receipt_calculator'
 
 def main str
@@ -6,33 +7,6 @@ def main str
   receipt = ReceiptCalculator.call order_lines
 
   print_receipt receipt
-end
-
-def parse_order_lines lines
-  regex = /^(\d+)\s+([\w\s]+)\sat\s+(\d+\.\d+)$/
-  lines = lines.split("\n")
-  raise_format_error if lines.empty?
-
-  lines.map do |line|
-    match = line.match(regex)
-    raise_format_error if match.nil? || match.length != 4
-
-    quantity, name, price = match.to_a[1..-1]
-
-    { quantity: quantity.to_i, name: name, price: price.to_f.round(2) }
-  end
-end
-
-def print_receipt receipt
-  receipt[:items].each do |item|
-    puts "#{item[:quantity]} #{item[:name]}: #{item[:total]}"
-  end
-  puts "Sales Taxes: #{receipt[:tax_total]}"
-  puts "Total: #{receipt[:total]}"
-end
-
-def raise_format_error
-  raise StandardError.new("Invalid input format")
 end
 
 begin
